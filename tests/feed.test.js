58af04e8e757d267d56f70bff662890545ecb422
hermanu <manuguionbajo@gmail.com>
2021-06-02 18:34:17 +0200

    Fixed .env implementation Added basic test structure Added basic initial test

---
+++ tests/feed.test.js
@@ -0,0 +1,19 @@
+const { assert, expect } = require("chai");
+const request = require("supertest");
+
+const app = require("../app");
+
+describe("Feed endpoints", () => {
+  it("GET /feed", async () => {
+    await request(app)
+      .get("/feed")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        console.log(res.body);
+        expect(text).to.not.be.undefined;
+        expect(text).to.be.an("object");
+      });
+  });
+});
