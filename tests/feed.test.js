1c9ce8d4a8c1a988532e0762268ad9861ad60866
hermanu <manuguionbajo@gmail.com>
2021-06-02 18:38:13 +0200

    Added additional conditions to initial test

--- tests/feed.test.js
+++ tests/feed.test.js
@@ -10,9 +10,11 @@ describe("Feed endpoints", () => {
       .set("Accept", "application/json")
       .expect("Content-Type", /json/)
       .expect(200)
-      .then((res) => {
-        expect(res.body).to.not.be.undefined;
-        expect(res.body).to.be.an("object");
+      .then(({ body }) => {
+        expect(body).to.not.be.undefined;
+        expect(body).to.be.an("object");
+        expect(body.data).to.be.an("array");
+        expect(body.data).length.to.be.above(0);
       });
   });
 });
