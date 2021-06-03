615e2ad98334493a92e539e664e2caeb0a7bd022
hermanu <manuguionbajo@gmail.com>
2021-06-03 17:34:16 +0200

    Added patch /:id route

--- src/routes/feed.route.js
+++ src/routes/feed.route.js
@@ -3,29 +3,43 @@ const router = express.Router();
 
 const Feed = require("../controllers/feed.controller");
 
+//Get today news, does scrap if needed
 router.get("/", async (req, res) => {
   try {
-    const newsFeed = await Feed.todayNews();
+    const newsFeed = await Feed.getNewsFeed();
     return res.status(200).json({ data: newsFeed });
   } catch (error) {
     console.log(error);
   }
 });
 
+// Create new Feed manually
 router.post("/", async (req, res) => {
   try {
     const newFeed = await Feed.createFeed(req.body);
-    res.status(200).json({ status: true, data: newFeed });
+    res.status(200).json({ data: newFeed });
   } catch (error) {
     console.log(error);
   }
 });
 
+//Delete feed by id
 router.delete("/:id", async (req, res) => {
   try {
     const id = req.params.id;
     const deleted = await Feed.deleteFeed(id);
     res.status(200).json({ data: deleted });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const feed = req.body;
+    const updatedFeed = await Feed.updateFeed(id, feed);
+    res.status(200).json({ data: updatedFeed });
   } catch (error) {
     console.log(error);
   }
